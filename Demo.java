d928965843c966ac778b14214b534e5a84d0f162
Rohit <gupta22kumar@gmail.com>
2018-10-02 08:19:11 -0700

    added java program

---
+++ Demo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        /* This reads the input provided by user
+         * using keyboard
+         */
+        // This method reads the number provided using keyboard
+        int num1 = 10;
+
+        int num2 = 20;
+
+        // Closing Scanner after the use
+
+        // Calculating product of two numbers
+        int product = num1+num2;
+
+        // Displaying the multiplication result
+        System.out.println("Output: "+product);
+    }
+}
